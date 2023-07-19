c5b67668396b7943610f5ec0eb6f25d706a1d1d0
Tute <mateoyastor60@gmail.com>
2023-07-19 11:51:46 -0300

    section 2 responsive

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,8 @@ import tute from '../../public/tutedev.png';
 
 export default function Home() {
   return (
-  <div>
-    <Head>
-      <title>Mateo Emilio Portafolio</title>
-      <meta name="descripction"></meta>
-    </Head>
-
     <main className="bg-white px-10">
+
       <section className="min-h-screen">
         <nav className="py-10 mb-12 flex justify-between" >
           <h1 className="text-xl font-burtons">developedbytute</h1>
@@ -25,6 +20,7 @@ export default function Home() {
             </li>
           </ul>
         </nav>
+
         <div className="text-center p-10">
           <h2 className="text-5xl py-2 text-teal-500 font-bold">Mateo Emilio</h2>
           <h3 className="text-2xl py-2 font-semibold">Systems engineering student</h3>
@@ -34,26 +30,48 @@ export default function Home() {
           lorem impsum lorem impsum lorem impsum  
           </p>
         </div>
+
         <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
           <AiFillTwitterCircle/>
           <AiFillLinkedin/>
         </div>
       </section>
 
-      <section className="min-h-screen">
-        <div className="m-3.5 w-70">
-            <Image className="min-w-0 max-w-sm rounded-full mt-5 w-2/5 float-right" src={tute}/>
-            <div>
-              <h2 className="w-3/5 text-5xl py-10 text-teal-500 font-bold">About me</h2>
-              <p className="w-3/5 py-10 leading-8 text-gray-800 dark:text-gray-200 max-w-xl md:text-xl float-left">
+      {/* <section className="min-h-screen">
+        <div className="m-3.5">
+            <Image className="w-2/5 min-w-fit max-w-fit max-h-fit h-screen rounded-full border-8 shadow-2xl float-right" src={tute}/>
+            <div className="flex-col w-3/5 min-w-sm max-w-full">
+              <h2 className="text-5xl py-10 text-teal-500 font-bold">About me</h2>
+              <p className="text-xl py-10 leading-8 text-gray-800 dark:text-gray-200  float-left">
               Blockchain enthusiast lorem impsum 
               lorem impsum lorem impsum lorem impsum 
+              lorem impsum lorem impsum lorem impsum
+              lorem impsum lorem impsum lorem impsum
               lorem impsum lorem impsum lorem impsum  
               </p>
             </div>
         </div>
+      </section> */}
+
+      <section className="grid gap-8 md:grid-cols-2 text-center md:text-left">
+
+        <div className="py-10">
+          <h2 className="text-5xl font-bold">About me</h2>
+          <p className="text-xl py-10 leading-8">
+            Blockchain enthusiast lorem impsum 
+            lorem impsum lorem impsum lorem impsum 
+            lorem impsum lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum  
+          </p>
+        </div>
+
+        <div className="sm:items-center">
+          <Image className="xl:max-h-fit md:float-right  rounded-full border-8 shadow-2xl" src={tute}/>
+        </div>
+        
       </section>
-      
+
       <section className="bg-red-700 min-h-screen">
         <div >
            
@@ -61,6 +79,6 @@ export default function Home() {
 
       </section>
     </main>
-  </div>
+  
   )
 }
