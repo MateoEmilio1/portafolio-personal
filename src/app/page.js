4384f1c359596f836fb0d739c3e1064eddd29e4e
MateoEmilio1 <mateoyastor60@gmail.com>
2023-07-25 00:41:22 -0300

    section communication

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,30 @@
+'use client'  
 import Head from 'next/head';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {AiFillTwitterCircle, AiFillLinkedin} from "react-icons/ai";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
 import Image from 'next/image';
 import tute from '../../public/tutedev.png';
+import charla1 from '../../public/charla1.jpg';
+import charla2 from '../../public/charla1.jpg';
+import charla3 from '../../public/charla1.jpg';
+import charla4 from '../../public/charla1.jpg';
+import charla5 from '../../public/charla1.jpg';
+
+
 
 export default function Home() {
   return (
+    
+
     <main className="bg-white px-10">
 
+    {/* SECTOR PRINCIPAL */}
       <section className="min-h-screen">
         <nav className="py-10 mb-12 flex justify-between" >
           <h1 className="text-xl font-burtons">developedbytute</h1>
@@ -25,7 +42,8 @@ export default function Home() {
           <h2 className="text-5xl py-2 text-teal-500 font-bold">Mateo Emilio</h2>
           <h3 className="text-2xl py-2 font-semibold">Systems engineering student</h3>
           <h3 className="text-xl py-2 font-semibold">Junior Developer</h3>
-          <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">Blockchain enthusiast lorem impsum 
+          <p className="text-md py-5 leading-8 text-gray-800 max-w-xl mx-auto md:text-xl">
+          Blockchain enthusiast lorem impsum 
           lorem impsum lorem impsum lorem impsum 
           lorem impsum lorem impsum lorem impsum  
           </p>
@@ -37,25 +55,11 @@ export default function Home() {
         </div>
       </section>
 
-      {/* <section className="min-h-screen">
-        <div className="m-3.5">
-            <Image className="w-2/5 min-w-fit max-w-fit max-h-fit h-screen rounded-full border-8 shadow-2xl float-right" src={tute}/>
-            <div className="flex-col w-3/5 min-w-sm max-w-full">
-              <h2 className="text-5xl py-10 text-teal-500 font-bold">About me</h2>
-              <p className="text-xl py-10 leading-8 text-gray-800 dark:text-gray-200  float-left">
-              Blockchain enthusiast lorem impsum 
-              lorem impsum lorem impsum lorem impsum 
-              lorem impsum lorem impsum lorem impsum
-              lorem impsum lorem impsum lorem impsum
-              lorem impsum lorem impsum lorem impsum  
-              </p>
-            </div>
-        </div>
-      </section> */}
+      {/* SECTOR ABOUT ME */}
 
-      <section className="grid gap-8 md:grid-cols-2 text-center md:text-left">
+      <section className="grid gap-8 min-h-screen md:grid-cols-2 text-center md:text-left">
 
-        <div className="py-10">
+        <div className="py-10 text-black">
           <h2 className="text-5xl font-bold">About me</h2>
           <p className="text-xl py-10 leading-8">
             Blockchain enthusiast lorem impsum 
@@ -72,10 +76,29 @@ export default function Home() {
         
       </section>
 
-      <section className="bg-red-700 min-h-screen">
-        <div >
-           
-        </div>
+      {/* SECTOR COMUNICACION + CARRUSEL */}
+
+      <section className="grid gap-8 md:grid-cols-2 text-center md:text-center">
+
+      {/* Hacer en este div el Slider */}  
+      <div className="sm:items-center">
+        <Image className="xl:max-h-fit md:float-right  rounded-full border-8 shadow-2xl" src={charla1}/>
+      </div> 
+      
+      
+
+      <div className="py-10 text-black">
+        <h2 className="text-5xl font-bold">Communication</h2>
+        <p className="text-xl py-10 leading-8">
+            Charla topic bla bla bla lorem impsum 
+            lorem impsum lorem impsum lorem impsum 
+            lorem impsum lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum  
+          </p>
+      </div>
+
+        
 
       </section>
     </main>
