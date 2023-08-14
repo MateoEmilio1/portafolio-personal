9d34850a9c7027171c7ed4e115122517aefabf8f
Tute <mateoyastor60@gmail.com>
2023-08-14 20:21:48 -0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -29,45 +29,49 @@ export default function Home({ Component, pageProps }) {
 
   return (
     
-    <main className="bg-white px-10 dark:bg-slate-900">
+    <main className="bg-white px-10 dark:bg-slate-800">
 
     {/* SECTOR PRINCIPAL */}
       
       <section className="min-h-screen">
         <nav className="py-10 mb-12 flex justify-between" >
-          <h1 className="text-xl font-burtons">poner algo aca?</h1>
+
+          <h1 className="text-xl font-burtons"></h1>
 
           <ul className="flex items-right">
-          {/* DARK THEME */}
-
-            <button className="bg-gray-700 text-white rounded-xl py-3 px-4" onClick={changeDarkMode} >
-              Dia/Noche
-            </button>
+       
 
             <li>
+                 {/* DARK THEME */}
+              <button 
+              className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-2 py-1.5 rounded-md ml-8" 
+              onClick={changeDarkMode} >
+                Noche/Dia
+              </button>
               <a 
               className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" 
-              href="https://doc-14-5c-prod-02-apps-viewer.googleusercontent.com/viewer2/prod-02/pdf/ss3rnlveuf8tv316bjvf008e5907cbt8/i6mi9nsdqnem77ljb4hr5psmp08o5stp/1690292475000/3/117746624012228893345/APznzaYIjy6VzabT78ZfjFYR0-GSQlTLvlbQFtxn4cX6qK7w9m3PJMuTzvSPe7sChQ_8eswdJvwYoMKTBRQdN6R0wGcHMW8QGM5mwnrL6G3OI_P0LlZkiFfd6scnZ7iw2I6UaHRW2R7EtUGJKHUDf_b-LYAzzttlsmbVYU5Dj-IpW2l1JX-59UXNhDoQw6sYgqTffg85X1t_7NFcPYvywFQSoyA_Jo7hbQsPZAJX3OKqIRSya0eNuFixsGs9uBGw48oT39-LfuEqzTA-XA4f_sGiibkyrYBKCWTGqsmGWC7lxFhth1lPeP4ATJmi7xNA8UWyTo2wMdgSi3OsTGwZpR8NoVnZnIOvGST39f1lkA0P_Yb7hSTWeyhEKh1EheEObhgo1Cn1LUNylLOrySQvNqj4Uvp12-7cjA==?authuser=0&nonce=aeohilr9fk9f0&user=117746624012228893345&hash=ekoaiq7v0cfbcsdubtkk0dr5lin71g6g"  
+              href="https://drive.google.com/drive/folders/1mVeYA3crYMvmJnp3LlYqR3O9F-67vzk_?usp=drive_link"  
               rel="noopener noreferrer"
               target="_blank"> {/* The _blank value specifies that the resource should be loaded into a new tab. */} 
               Curriculum
               </a>
             </li>
+
           </ul>
         </nav>
 
         <div className="text-center p-10">
           <h2 className="text-5xl py-2 text-teal-500 font-bold">Mateo Emilio</h2>
-          <h3 className="text-2xl py-2 font-semibold">Systems engineering student</h3>
-          <h3 className="text-xl py-2 font-semibold">Junior Developer</h3>
-          <p className="text-md py-5 leading-8 text-gray-800 max-w-xl mx-auto md:text-xl">
+          <h3 className="text-2xl py-2 font-semibold dark:text-white">Systems engineering student</h3>
+          <h3 className="text-xl py-2 font-semibold dark:text-white">Junior Developer</h3>
+          <p className="text-md py-5 leading-8 text-gray-800 max-w-xl mx-auto md:text-xl dark:text-white">
           Blockchain enthusiast lorem impsum 
           lorem impsum lorem impsum lorem impsum 
           lorem impsum lorem impsum lorem impsum  
           </p>
         </div>
 
-        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white">
           <a href="https://linkedin.com/in/MateoEmilio1"
           rel="noopener noreferrer"
           target="_blank">
@@ -88,9 +92,9 @@ export default function Home({ Component, pageProps }) {
       
       <section className="grid gap-8 min-h-screen md:grid-cols-2 text-center md:text-left">
 
-        <div className="py-20 text-black">
-          <h2 className="text-5xl font-bold">About me</h2>
-          <p className="text-xl py-10 leading-8">
+        <div className="py-20 text-black dark:text-white">
+          <h2 className="text-5xl font-bold e">About me</h2>
+          <p className="text-xl py-10 leading-8 ">
             Blockchain enthusiast lorem impsum 
             lorem impsum lorem impsum lorem impsum 
             lorem impsum lorem impsum lorem impsum
@@ -158,7 +162,7 @@ export default function Home({ Component, pageProps }) {
       
       
 
-      <div className="py-1s text-black">
+      <div className="py-1s text-black dark:text-white">
         <h2 className="text-4xl font-bold">Communication</h2>
         <p className="text-xl py-10 leading-8">
             Charla topic bla bla bla lorem impsum 
