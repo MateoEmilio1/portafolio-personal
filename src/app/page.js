938e7cace688273369a8f425ab2e4c408fc354ba
MateoEmilio1 <mateoyastor60@gmail.com>
2023-08-17 16:23:49 -0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -31,15 +31,6 @@ export default function Home({ Component, pageProps }) {
     
     <main className="bg-white px-10 dark:bg-slate-800">
       
-      {/* <!-- Google tag (gtag.js) --> */}
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-6XEJWYS2R3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-
-  gtag('config', 'G-6XEJWYS2R3');
-</script>
 
     {/* SECTOR PRINCIPAL */}
       
