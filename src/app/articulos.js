fd539fb797946997b1f6ef005f7118294c34720b
Tute <mateoyastor60@gmail.com>
2023-09-28 10:45:37 -0300

    seccion articulos

---
+++ src/app/articulos.js
@@ -0,0 +1,15 @@
+export default[
+  {
+    "img": "/images/articulo-guia-ethereum.png",
+    "title": "Nodo Ethereum",
+    "text": "Guia de como configurar un nodo de Ethereum, desde la instalación hasta la sincronización utilizando DappNode.",
+    "link": "https://mirror.xyz/seedlatam.eth/VpuKM5vy2uWpK-H-MVGcbZaCIlRVoC3iTsASDDXIhTY"
+  },
+  {
+    "img": "/images/articulo-guia-arbitrum.jpg",
+    "title": "Nodo Arbitrum",
+    "text": "Guia de como configurar un nodo de Arbitrum Nitro, utilizando una VM y Docker con sus configuraciones y como interactuar él.",
+    "link": "https://mirror.xyz/layer2es.eth/J_kUr6roi4IVEuZXT44rfxO8L9n-yBZpPkOadrElPtw"
+  },
+ 
+]
