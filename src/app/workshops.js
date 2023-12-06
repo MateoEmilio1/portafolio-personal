6311aab57cb47077dde2052d8f73ee694eba1911
Tute <mateoyastor60@gmail.com>
2023-12-06 14:10:54 -0300

    update workshop bitconf

--- src/app/workshops.js
+++ src/app/workshops.js
@@ -1,26 +1,24 @@
 export default [
     {
-        "img" : "/images/charla1.png",
-        "muestra" : () => window.my_modal_1.showModal(),
+        "img" : "/images/charlaBitconf.jpg",
+        "muestra" : () => window.my_modal_bitconf.showModal(),
     },
     {
-        "img" : "/images/charla2.png",
-        "muestra" : () => window.my_modal_2.showModal(),
+        "img" : "/images/charla5.jpg",
+        "muestra" : () => window.my_modal_5.showModal(),
     },
     {
         "img" : "/images/charla3.png",
         "muestra" : () => window.my_modal_3.showModal(),
     },
     {
-        "img" : "/images/charla4.png",
-        "muestra" : () => window.my_modal_4.showModal(),
+        "img" : "/images/charla2.png",
+        "muestra" : () => window.my_modal_2.showModal(),
     },
     {
-        "img" : "/images/charla5.jpg",
-        "muestra" : () => window.my_modal_5.showModal(),
+        "img" : "/images/charla1.png",
+        "muestra" : () => window.my_modal_1.showModal(),
     },
-
-
 
 
 ]
